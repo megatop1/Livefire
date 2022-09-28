b4ad66a9ba3ea64fcd679f05e83738b75471af5e
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-09-27 21:14:51 -0400

    Create DeployOU.ps1

---
+++ Windows/Cali/DeployOU.ps1
@@ -0,0 +1,6 @@
+New-ADOrganizationalUnit -Name "Don" -Path "DC=CALI,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
+New-ADOrganizationalUnit -Name "Enforcer" -Path "DC=CALI,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
+New-ADOrganizationalUnit -Name "Security" -Path "DC=CALI,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
+New-ADOrganizationalUnit -Name "Mule" -Path "DC=CALI,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
+New-ADOrganizationalUnit -Name "Accountant" -Path "DC=CALI,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
+New-ADOrganizationalUnit -Name "Corrupt Cop" -Path "DC=CALI,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
