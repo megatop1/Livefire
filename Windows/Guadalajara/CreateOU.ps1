81d73c4cbde9fa25c68f2b4ecfc534182e8d8be3
megatop1 <43348736+megatop1@users.noreply.github.com>
2022-09-27 22:33:02 -0400

    Create CreateOU.ps1

---
+++ Windows/Guadalajara/CreateOU.ps1
@@ -0,0 +1,6 @@
+New-ADOrganizationalUnit -Name "Don" -Path "DC=GUADALAJARA,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
+New-ADOrganizationalUnit -Name "Enforcer" -Path "DC=GUADALAJARA,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
+New-ADOrganizationalUnit -Name "Security" -Path "DC=GUADALAJARA,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
+New-ADOrganizationalUnit -Name "Mule" -Path "DC=GUADALAJARA,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
+New-ADOrganizationalUnit -Name "Accountant" -Path "DC=GUADALAJARA,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
+New-ADOrganizationalUnit -Name "Corrupt Cop" -Path "DC=GUADALAJARA,DC=CARTEL" -ProtectedFromAccidentalDeletion $True
